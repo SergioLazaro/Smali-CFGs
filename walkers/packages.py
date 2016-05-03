1f0b37b177a3cc6714c85782c3b639a27fa602ed
SergioLazaro <sergiozgz1994@gmail.com>
2016-05-03 11:39:14 +0200

    testing

--- walkers/packages.py
+++ walkers/packages.py
@@ -4,15 +4,15 @@
 import re
 
 class PackagesFinder(object):
-	def __init__(self, what):
-		self.target_package = what
+    def __init__(self, what):
+        self.target_package = what
 
-	def do_find(self, where):
-		co = []
-		for class_name in where:
-			for mth_dict in where[class_name]['Methods']:
-				caller = "%s->%s" % (class_name, mth_dict['Name'])
-				block = '\n'.join( mth_dict['Instructions'] )
-				for pair in re.findall('%s/(.*?);\->(.*?)\n' % (self.target_package), block):
-					co.append( ';->'.join(pair) )
-		return sorted(list(set(co)))
+    def do_find(self, where):
+        co = []
+        for class_name in where:
+            for mth_dict in where[class_name]['Methods']:
+                caller = "%s->%s" % (class_name, mth_dict['Name'])
+                block = '\n'.join( mth_dict['Instructions'] )
+                for pair in re.findall('%s/(.*?);\->(.*?)\n' % (self.target_package), block):
+                    co.append( ';->'.join(pair) )
+        return sorted(list(set(co)))
